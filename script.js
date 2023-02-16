6bccba9437cc832d58d1c01b787d71f8fa51a650
Daniel Kahoun <kahoun@SPSEJECNANET.CZ>
2023-02-16 07:45:06 +0100

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+class Employee {
+    constructor(name, position, salary, start_date, office, extn) {
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+        this.start_date = start_date;
+        this.office = office;
+        this.extn = extn;
+    }
+}
